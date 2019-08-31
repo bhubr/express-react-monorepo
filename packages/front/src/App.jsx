2b224ec6871675936fb02a53969c7e93d3d361ad
Benoît Hubert <benoithubert@gmail.com>
2019-08-31 23:47:37 +0200

    Connect back&front apps, serve React build from Express
    * Fetch data from the backend in frontend app
    * Serve the React production build from the Express app

--- packages/front/src/App.jsx
+++ packages/front/src/App.jsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [foo, setFoo] = useState('N/A');
+  useEffect(
+    () => {
+      fetch('/api/foo')
+        .then((res) => res.json())
+        .then((data) => setFoo(data.foo))
+        .catch((err) => setFoo(err.message));
+    },
+  );
   return (
     <div>
       <h1>Hello World</h1>
+      <p>
+Server responded with foo:
+        {foo}
+      </p>
     </div>
   );
 }
