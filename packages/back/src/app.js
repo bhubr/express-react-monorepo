5fc7f70cc4cb0291bfc7ed8ca3d2cb630bbacec1
Benoît Hubert <benoithubert@gmail.com>
2019-11-07 04:02:27 +0100

    back to working status

--- packages/back/src/app.js
+++ packages/back/src/app.js
@@ -9,8 +9,7 @@ const isProd = process.env.NODE_ENV === 'production';
 const app = express();
 app.use(bodyParser.json());
 
-// app.get('/api/foo', (req, res) => res.json({ foo: 'bar' }));
-app.get('/api/foo', (req, res) => { throw new Error('error'); });
+app.get('/api/foo', (req, res) => res.json({ foo: 'bar' }));
 
 if (isProd) {
   // Compute the build path and index.html path
